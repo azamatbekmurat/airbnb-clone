ead1e931b82eab1595eb387cf4b344b317a8f363
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-05-16 13:42:53 -0700

    initial commit

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import { BrandingWatermark } from '@material-ui/icons'
+import React from 'react'
+import './Home.css'
+import Banner from "./Banner";
+
+function Home() {
+    return (
+        <div className="home">
+
+            <Banner />
+        </div>
+    )
+}
+
+export default Home
