57854aac188fdda5e3ec5c7ccaa59970047469fc
Azamat Bekmuratov <azamat.bekmuratov24@gmail.com>
2021-05-18 00:26:00 -0700

    add search result component

--- src/App.js
+++ src/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import './App.css';
 import Home from './Home';
 import Header from './Header';
-import Footer from "./Footer"
+import Footer from "./Footer";
+import SearchPage from "./SearchPage"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 function App() {
   return (
 
     //BEM
     <div className="app">
-      <Header />
-      <Home/>
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route exact path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
 
     
